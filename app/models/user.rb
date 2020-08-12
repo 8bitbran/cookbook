e4ac8cdaadf12e93f2e84d1e11429d07e6f57943
8bitbran <brandonmar97@gmail.com>
2020-08-11 21:45:10 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 16 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 8, maximum: 32 }
+    has_secure_password
 end
