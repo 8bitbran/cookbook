57ef30df8f868b08bfadc3b3e2d526baac1b11a9
8bitbran <brandonmar97@gmail.com>
2020-08-13 10:46:41 -0400

    Add associations and validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,14 @@ class Recipe < ApplicationRecord
     belongs_to :user
     belongs_to :category
     has_many :recipe_ingredients
-    has_many :ingredients, through: :recipe_ingredients
+	has_many :ingredients, through: :recipe_ingredients
+    accepts_nested_attributes_for :ingredients,  allow_destroy: true, reject_if: :all_blank
+    accepts_nested_attributes_for :recipe_ingredients
+    validates :title, presence: true, length: { minimum: 4 }
+    validates :description, presence: true, length: { in: 4..500 }
+    validates :directions, presence: true, length: { minimum: 4 }
+    validates :prep_time, presence: true
+    validates :cook_time, presence: true
+    validates :category_id, presence: true
+    validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 16 }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 8, maximum: 32 }
+    has_one_attached :avatar
     has_secure_password
 end
