ac8d1ca0c18577975132ad19b5a9638cc98d6f74
8bitbran <brandonmar97@gmail.com>
2020-08-13 10:45:04 -0400

    Add recipe actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,49 @@
 class RecipesController < ApplicationController
+
+    def index
+        @recipes = Recipe.all
+    end 
+
+    def new
+        @recipe = Recipe.new
+        @ingredients = @recipe.ingredients.build
+    end 
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save 
+            redirect_to recipe_path(@recipe.id)
+        else 
+            render 'new'
+        end
+    end 
+
+    def edit
+
+    end 
+
+    def update
+
+    end 
+
+    def destroy
+
+    end 
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end 
+
+    def recipe_params
+        params.require(:recipe).permit(
+        :title, 
+        :description,  
+        :directions, 
+        :prep_time, 
+        :cook_time, 
+        :servings, 
+            ingredients_attributes: [:id, :name, :_destroy])
+    end 
 end
