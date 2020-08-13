b0f19880397e0a7bfede82535c6967ca0be8cae7
8bitbran <brandonmar97@gmail.com>
2020-08-13 10:49:05 -0400

    Add route for recipes#create

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :likes
   resources :categories
+  post '/recipes', to: 'recipes#create'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
