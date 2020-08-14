0986d0e07da401f01071d6d19c22018835efce5b
8bitbran <brandonmar97@gmail.com>
2020-08-14 10:43:40 -0400

    Add ingredient_name= to find/create ingredients

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,4 +2,17 @@ class RecipeIngredient < ApplicationRecord
     belongs_to :recipe
     belongs_to :ingredient
     validates :quantity, presence: true
+
+    def ingredient_name=(name)
+        ingredient = Ingredient.find_or_create_by(name: name)
+        self.ingredient_id = ingredient.id
+    end 
+
+    def ingredient_name
+        if ingredient
+            ingredient.name 
+        else
+            nil 
+        end
+    end 
 end
