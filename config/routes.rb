57ac394357e28f8629e0cd76c2cd150be24e3c7e
8bitbran <brandonmar97@gmail.com>
2020-08-14 11:39:10 -0400

    New facebook callback route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :likes
   resources :categories
   post '/recipes', to: 'recipes#create'
+  get '/auth/facebook/callback' => 'sessions#create'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
