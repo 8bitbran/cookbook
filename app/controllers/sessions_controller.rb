cce2b050a18277449370ac57144c39fdf92f7cff
8bitbran <brandonmar97@gmail.com>
2020-08-14 11:41:34 -0400

    Create action allows login with facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,27 @@ class SessionsController < ApplicationController
     end 
 
     def create
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user.id)
+        if auth
+            #facebook login flow
+            def create
+                @user = User.find_or_create_by(uid: auth['uid']) do |u|
+                  u.name = auth['info']['name']
+                  u.email = auth['info']['email']
+                  u.image = auth['info']['image']
+                end
+             
+                session[:user_id] = @user.id
+             
+                redirect_to user_path(@user)
         else 
-            render 'new'
+            #normal login flow
+            @user = User.find_by(username: params[:username])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else 
+                render 'new'
+            end 
         end 
     end 
 
@@ -18,5 +33,11 @@ class SessionsController < ApplicationController
         session.delete :user_id
         redirect_to '/login'
     end 
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
 
 end
