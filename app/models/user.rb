5042f78907768e51cc7598edffaad6833c49eb8a
8bitbran <brandonmar97@gmail.com>
2020-08-15 12:09:53 -0400

    Change username to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 16 }
+    validates :name, presence: true, length: { minimum: 3, maximum: 32 }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 8, maximum: 32 }
     has_one_attached :avatar
