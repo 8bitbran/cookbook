aed233bdec7086642ab0dae2c0fc55f1f44b2c61
8bitbran <brandonmar97@gmail.com>
2020-08-15 12:11:24 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root 'static#home'
   get '/profile', to: 'users#profile', as: 'profile'
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
   resources :ingredients
   resources :users, only: [:index, :show, :create, :edit]
-  resources :recipes, only: [:index, :show, :new, :edit, :destroy]
-  resources :comments
-  resources :likes
-  resources :categories
+  resources :recipes do
+    resources :comments, only: %i[create]
+    resources :likes, only: %i[create]
+  end
+  resources :categories, only: [:show]
   post '/recipes', to: 'recipes#create'
   get '/auth/facebook/callback' => 'sessions#create'
   
