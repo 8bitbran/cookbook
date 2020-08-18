a915ad350e79257ab769169c668ff51a162a98ee
8bitbran <brandonmar97@gmail.com>
2020-08-18 10:10:32 -0400

    Create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+    def create
+        @recipe = Recipe.find(params[:recipe_id])
+        @comment = @recipe.comments.create(comment_params)
+        @comment.user_id = current_user.id 
+        if @comment.save
+            redirect_to @recipe
+        else
+            flash.now[:danger] = "error"
+        end
+    end 
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:description)
+    end 
 end
