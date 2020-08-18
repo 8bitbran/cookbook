34934ee3adba020043dccb5fb3c695e9be39422a
8bitbran <brandonmar97@gmail.com>
2020-08-18 10:10:41 -0400

    Create likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+    def create
+        @recipe = Recipe.find(params[:recipe_id])
+        @like = @recipe.likes.create
+        @like.user_id = current_user.id 
+        if @like.save
+            redirect_to @recipe
+        else
+            flash.now[:danger] = "error"
+        end
+    end 
 end
