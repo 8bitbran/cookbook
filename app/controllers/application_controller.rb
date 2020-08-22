cf69705ac8fa47858ffb606c718d199cd51dfb73
8bitbran <brandonmar97@gmail.com>
2020-08-22 08:11:51 -0600

    Add require_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,14 @@ class ApplicationController < ActionController::Base
     def logged_in?
          !!current_user
     end
+ 
+    private 
+
+    def require_login
+        unless logged_in?
+            flash[:error] = "You must be logged in to access this section"
+            redirect_to signin_path # halts request cycle
+        end
+    end
 
 end
