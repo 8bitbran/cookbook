762c1a39f9263e941480a5d2fac4361734bc6587
8bitbran <brandonmar97@gmail.com>
2020-08-23 09:35:21 -0600

    Fixed user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params 
-        params.require(:user).permit(:name, :email, :password, :avatar)
+        params.require(:user).permit(:name, :email, :password, :image)
     end 
 
 end
