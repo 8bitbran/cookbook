135d7ae0ccc5be04352f0c4f542d9d235f486c52
8bitbran <brandonmar97@gmail.com>
2020-08-23 09:35:47 -0600

    Like/user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     has_many :recipes
+    has_many :likes
+    has_many :comments
     validates :name, presence: true, length: { minimum: 3, maximum: 32 }
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :password, presence: true, length: { minimum: 8, maximum: 32 }
-    has_one_attached :avatar
+    has_one_attached :image
     has_secure_password
 end
